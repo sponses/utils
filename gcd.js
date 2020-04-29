c6b12fedf385d0b048a25f07b29c2adbe0f33459
滕怡君 <1057120672@qq.com>
2020-04-29 15:39:47 +0800

    gcd

---
+++ gcd.js
@@ -0,0 +1,23 @@
+/**
+ * 辗转相除法
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function gcd_division(a, b) {
+  if (b === 0) return b
+  return gcd_division(b, a % b)
+}
+
+/**
+ * 更相减损术
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function gcd_miuns(a, b) {
+  if (a === b) return a
+  return a > b ? gcd_miuns(a - b, b) : gcd_miuns(a, b - a)
+}
+
+export default { gcd_division, gcd_miuns }
